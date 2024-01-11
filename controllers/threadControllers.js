e3efec1d531a3c024cedd3d2466696c795431ef9
Sary Zeaiter <saryzeaiter@gmail.com>
2024-01-11 22:26:47 +0100

    Fixed createThread issue

--- controllers/threadControllers.js
+++ controllers/threadControllers.js
@@ -26,6 +26,7 @@ const findThreadByPk = (req, res) => {
 }
 
 const createThread = (req, res) => {
+   
    const newThread = {...req.body, UserId: req.userId}
    Thread.create(newThread)
       .then((thread) => {
